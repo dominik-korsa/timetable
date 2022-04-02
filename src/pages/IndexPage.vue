544057e7779f8308cd1cc61eec9051cdb83490b6
Dominik Korsa <dominik.korsa@gmail.com>
2022-04-02 17:21:20 +0200

    Style V LO button

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -6,7 +6,7 @@
     <q-card
       flat
       bordered
-      class="optivum-picker"
+      class="index-page__optivum-picker"
     >
       <q-form
         greedy
@@ -74,9 +74,45 @@
       </template>
     </q-card>
 
-    <q-btn :to="{ name: 'VLo/SelectClass' }">
-      V LO
-    </q-btn>
+    <router-link
+      :to="{ name: 'VLo/SelectClass' }"
+      class="index-page__v-lo-link q-mt-md"
+    >
+      <q-card
+        class="index-page__v-lo"
+        outlined
+        dark
+        flat
+      >
+        <q-card-section horizontal>
+          <img
+            src="/v-lo.png"
+            alt="Logo V LO"
+            class="index-page__v-lo-logo"
+          >
+          <q-separator
+            vertical
+            dark
+          />
+          <q-card-section class="column justify-center index-page__v-lo-section">
+            <div class="text-h6 index-page__v-lo-name text-center">
+              V&nbsp;LO
+              <span class="index-page__v-lo-patron">im. Augusta Witkowskiego</span>
+              w&nbsp;Krakowie
+            </div>
+          </q-card-section>
+        </q-card-section>
+      </q-card>
+    </router-link>
+    <div class="text-caption q-mt-xs">
+      <q-icon
+        name="favorite"
+        class="q-mr-xs"
+        color="red"
+      />
+      <a href="https://github.com/cloud11665/vlo.rocks-api">API planu V LO</a>
+      by <a href="https://github.com/cloud11665">cloud11665</a>
+    </div>
   </q-page>
 </template>
 
@@ -148,9 +184,41 @@ export default defineComponent({
 
 <style lang="scss">
   .index-page {
-    .optivum-picker {
+    .index-page__optivum-picker {
       width: 100%;
       max-width: 600px;
+    }
+
+    .index-page__v-lo-link {
+      text-decoration: none;
+      color: inherit;
+
+      .index-page__v-lo {
+        background-color: #852439;
+
+        .index-page__v-lo-logo {
+          height: 110px;
+          max-height: 20vw;
+          aspect-ratio: 1;
+          align-self: center;
+        }
+
+        .index-page__v-lo-section {
+          flex: 1;
+        }
+
+        @media (max-width: $breakpoint-xs-max) {
+          .index-page__v-lo-name {
+            font-size: 1rem;
+          }
+        }
+
+        @media (max-width: 350px) {
+          .index-page__v-lo-patron {
+            display: none;
+          }
+        }
+      }
     }
   }
 </style>
