b5cf419e1adffeefad0fb46b6ad32bc6972b8099
Dominik Korsa <dominik.korsa@gmail.com>
2023-01-19 13:25:48 +0100

    Add OPTIVUM trademark notice

--- src/components/BuildInfo.vue
+++ src/components/BuildInfo.vue
@@ -1,39 +1,52 @@
 <template>
   <div class="build-info text-caption text-center">
-    <template v-if="config.superSecretSettingsEnabled">
+    <p v-if="config.superSecretSettingsEnabled">
       <router-link to="/super-secret-settings">
         Super Secret Settings
       </router-link>
-      <br>
-    </template>
+    </p>
 
-    <q-icon
-      name="favorite"
-      class="q-mr-xs"
-      color="red"
-    />
-    <a href="https://github.com/cloud11665/vlott">API planu V LO</a>
-    by <a href="https://github.com/cloud11665">cloud11665</a><br>
+    <p>
+      <q-icon
+        name="favorite"
+        class="q-mr-xs"
+        color="red"
+      />
+      <a href="https://github.com/cloud11665/vlott">API planu V LO</a>
+      by <a href="https://github.com/cloud11665">cloud11665</a>
+    </p>
 
-    <a
-      href="https://github.com/dominik-korsa/timetable"
-      @click="onSourceCodeLinkClick"
-    >Kod źródłowy aplikacji na GitHub</a><br>
+    <p>
+      <a
+        href="https://github.com/dominik-korsa/timetable"
+        @click="onSourceCodeLinkClick"
+      >Kod źródłowy aplikacji na GitHub</a>
+    </p>
 
-    <span
+    <p>
+      <i>OPTIVUM</i> jest zastrzeżonym znakiem<br>
+      handlowym firmy <i>VULCAN sp. z o.o.</i>
+    </p>
+
+    <p
       class="build-time"
       role="button"
       @click.prevent="buildTimeClick"
     >
-      Zbudowano: <b>{{ buildTimeString }}</b><br>
-    </span>
+      Zbudowano: <b>{{ buildTimeString }}</b>
+    </p>
+
     <template v-if="buildInfo !== null">
-      Deploy ID: <b><a
-        :href="`https://app.netlify.com/sites/${buildInfo.siteName}/deploys/${buildInfo.deployId}`"
-      >{{ buildInfo.deployId }}</a></b><br>
-      Commit: <b>{{ buildInfo.branch }}@<a
-        :href="`${buildInfo.repositoryUrl}/commit/${buildInfo.commitRef}`"
-      >{{ buildInfo.commitRef.substring(0, 7) }}</a></b><br>
+      <p>
+        Deploy ID: <b><a
+          :href="`https://app.netlify.com/sites/${buildInfo.siteName}/deploys/${buildInfo.deployId}`"
+        >{{ buildInfo.deployId }}</a></b>
+      </p>
+      <p>
+        Commit: <b>{{ buildInfo.branch }}@<a
+          :href="`${buildInfo.repositoryUrl}/commit/${buildInfo.commitRef}`"
+        >{{ buildInfo.commitRef.substring(0, 7) }}</a></b>
+      </p>
     </template>
   </div>
 </template>
@@ -97,7 +110,15 @@ export default defineComponent({
 
 <style lang="scss">
 .build-info {
-  line-height: 2;
+  line-height: 1.2;
+
+  p {
+    margin-bottom: 0.75em;
+  }
+
+  p:last-of-type {
+    margin-bottom: 0;
+  }
 
   .build-time {
     user-select: none;
