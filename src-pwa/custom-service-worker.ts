7ed91b54d4ec048f2747e56834bacb8802fd99bf
Dominik Korsa <dominik.korsa@gmail.com>
2023-04-28 18:26:34 +0200

    Fix PWA not working when offline

--- src-pwa/custom-service-worker.ts
+++ src-pwa/custom-service-worker.ts
@@ -4,7 +4,9 @@
  * quasar.config.js > pwa > workboxPluginMode is set to "InjectManifest"
  */
 
-import { precacheAndRoute } from 'workbox-precaching';
+import { clientsClaim } from 'workbox-core';
+import { precacheAndRoute, cleanupOutdatedCaches, createHandlerBoundToURL } from 'workbox-precaching';
+import { registerRoute, NavigationRoute } from 'workbox-routing';
 import { showNotification } from 'src/push/push-notifier';
 import { SubstitutionNotificationData } from 'src/push/push-types';
 import { getCombinedTimetableUrl } from 'src/router/route-constants';
@@ -12,8 +14,22 @@ import { getCombinedTimetableUrl } from 'src/router/route-constants';
 declare let self: ServiceWorkerGlobalScope;
 export {};
 
+self.skipWaiting().catch();
+clientsClaim();
+
 // Use with precache injection
 precacheAndRoute(self.__WB_MANIFEST);
+
+cleanupOutdatedCaches();
+
+if (process.env.MODE !== 'ssr' || process.env.PROD) {
+  registerRoute(
+    new NavigationRoute(
+      createHandlerBoundToURL(process.env.PWA_FALLBACK_HTML!),
+      { denylist: [/sw\.js$/, /workbox-(.)*\.js$/] },
+    ),
+  );
+}
 
 self.addEventListener('push', (event) => {
   if (!event.data) {
@@ -29,12 +45,4 @@ self.addEventListener('notificationclick', (event) => {
   if (!notificationData) return;
   event.notification.close();
   event.waitUntil(self.clients.openWindow(getCombinedTimetableUrl(notificationData.tri, notificationData.date)));
-});
-
-self.addEventListener('install', () => {
-  self.skipWaiting().catch();
-});
-
-self.addEventListener('activate', (event) => {
-  event.waitUntil(self.clients.claim());
 });
