c53f772343331372261e5d5889aa59f1458c0e2b
Dominik Korsa <dominik.korsa@gmail.com>
2023-08-26 10:51:16 +0200

    Add room and teacher search

--- src/components/lists/UnitList.vue
+++ src/components/lists/UnitList.vue
@@ -13,14 +13,38 @@
     v-else
     flat
     bordered
-    class="overflow-auto"
+    class="column"
   >
+    <q-input
+      v-model="searchValue"
+      borderless
+      class="q-px-md border-b"
+      :placeholder="searchMessage"
+      clearable
+      @clear="searchValue = ''"
+      @keydown.esc="searchValue = ''"
+    >
+      <template #append>
+        <q-icon
+          v-if="searchValue === ''"
+          name="search"
+        />
+      </template>
+    </q-input>
+    <div
+      v-if="filteredItems.length === 0"
+      class="q-pa-md text-reduced text-center"
+    >
+      Brak wyników
+    </div>
     <q-list
+      v-else
       dense
       separator
+      class="row-fill overflow-auto"
     >
       <q-item
-        v-for="item in items"
+        v-for="item in filteredItems"
         :key="item.unit"
         :to="item.to"
       >
@@ -31,18 +55,24 @@
 </template>
 
 <script lang="ts" setup>
-import { computed } from 'vue';
+import { computed, ref } from 'vue';
 import { UnitListItem } from 'src/api/client';
 import { useRoute } from 'vue-router';
 import { paramNames, routeNames } from 'src/router/route-constants';
+import _ from 'lodash';
 
 const props = defineProps<{
   units: UnitListItem[];
   unitType: 'teacher' | 'room';
 }>();
 
+const searchValue = ref('');
+
 const emptyMessage = computed(
   () => `Ta szkoła nie udostępnia planów ${props.unitType === 'teacher' ? 'naczycieli' : 'sal'}`,
+);
+const searchMessage = computed(
+  () => `Wyszukaj ${props.unitType === 'teacher' ? 'naczyciela' : 'salę'}`,
 );
 
 const route = useRoute();
@@ -56,5 +86,29 @@ const items = computed(() => props.units.map((unit) => ({
       [paramNames.unit]: unit.unit,
     },
   },
+  searchText: unit.name.toLocaleLowerCase('pl').replaceAll(/\s/g, ''),
 })));
+
+const filteredItems = computed(() => {
+  // TODO: Implement better search for teachers, taking into account initials
+  const parts = searchValue.value
+    .split(/\s/)
+    .map((part) => part.toLocaleLowerCase('pl').trim())
+    .filter((part) => part !== '');
+  if (parts.length === 0) return items.value;
+  let highestMatchCount = 1;
+  return _.orderBy(
+    items.value.map((item) => {
+      const matchCount = parts.filter((part) => item.searchText.includes(part)).length;
+      if (matchCount > highestMatchCount) highestMatchCount = matchCount;
+      return ({
+        ...item,
+        matchCount,
+      });
+    }),
+    ['matchCount'],
+    ['desc'],
+  )
+    .filter(({ matchCount }) => matchCount * 2 >= highestMatchCount);
+});
 </script>
