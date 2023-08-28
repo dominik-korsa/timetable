da3ac5536f29e1698e1e3ec7420a39a88f21123e
Dominik Korsa <dominik.korsa@gmail.com>
2023-08-28 22:17:15 +0200

    Add more parts to TRI

--- src/router/tri.ts
+++ src/router/tri.ts
@@ -1,27 +1,43 @@
 const anyOf = (...parts: string[]) => `(?:${parts.join('|')})`;
 const optional = (part: string) => `(?:${part})?`;
 
-const vLo = 'v-lo';
+const timetableLiteral = 'plan';
+
+const schoolSlugs = {
+  'v-lo': 4735,
+};
+
+const vLo = anyOf(...Object.keys(schoolSlugs));
 
 const optivum = 'o,[012],[^\\/,]+,[^\\/,]+';
 
 const rspo = '\\d+';
 
 const slash = '\\/';
-const part = (prefix: string) => `${prefix}\\.[^\\/\\.,]+`;
+const slug = '[^\\/\\.,]+';
+const part = (prefix: string) => `${prefix}\\.${slug}`;
+
 const voivodeship = part('w') + slash;
 const county = part('p') + slash;
 const city = part('m');
 const commune = part('gm');
-const school = `${slash}[^\\/\\.,]+`;
+const schoolOrRspo = slug;
+
+const url = '\'[^\']\'';
+
+const version = '\\+\\d+';
 
 export const triRegex = anyOf(
   vLo,
   optivum,
-  rspo,
-  optional(voivodeship) + anyOf(
-    optional(county) + city,
-    county + commune,
-  ) + school,
+  url,
+  timetableLiteral + version,
+  anyOf(
+    rspo,
+    optional(voivodeship) + anyOf(
+      optional(county) + city,
+      county + commune,
+    ) + slash + schoolOrRspo,
+  ) + optional(version),
 );
 console.log('Tri regex:', triRegex);
