3d748a4df492105c60f588d4c6d7513d849f09c8
Dominik Korsa <dominik.korsa@gmail.com>
2023-09-09 12:54:25 +0200

    Refactor App.vue to use <script setup>

--- src/App.vue
+++ src/App.vue
@@ -3,9 +3,9 @@
   <router-view v-else />
 </template>
 
-<script lang="ts">
+<script lang="ts" setup>
 import {
-  computed, defineComponent, provide, ref, watch,
+  computed, provide, ref, watch,
 } from 'vue';
 import { useQuasar } from 'quasar';
 import { useConfigStore } from 'stores/config';
@@ -14,51 +14,43 @@ import { useRoute, useRouter } from 'vue-router';
 import { Client, clientSymbol, getClient } from 'src/api/client';
 import { paramNames, routeNames } from 'src/router/route-constants';
 
-export default defineComponent({
-  name: 'App',
-  components: { OldDomainLayout },
-  setup: () => {
-    const quasar = useQuasar();
-    const config = useConfigStore();
+const quasar = useQuasar();
+const config = useConfigStore();
+const router = useRouter();
+const route = useRoute();
 
-    const router = useRouter();
-    const route = useRoute();
-    const client = ref<Client | undefined>();
-    provide(clientSymbol, client);
-    watch(() => route.params[paramNames.tri], (tri: string | string[] | undefined, oldTri) => {
-      if (tri === oldTri || tri === undefined) return;
-      if (typeof tri === 'object') [tri] = tri;
-      try {
-        client.value = getClient(tri);
-      } catch (error) {
-        console.error(error);
-        quasar.notify({
-          type: 'negative',
-          message: 'Niepoprawny identyfikator planu lekcji',
-        });
-        router.push({ name: routeNames.home });
-        client.value = undefined;
-      }
-    }, { immediate: true });
+const client = ref<Client | undefined>();
+provide(clientSymbol, client);
+watch(() => route.params[paramNames.tri], (tri: string | string[] | undefined, oldTri) => {
+  if (tri === oldTri || tri === undefined) return;
+  if (typeof tri === 'object') [tri] = tri;
+  try {
+    client.value = getClient(tri);
+  } catch (error) {
+    console.error(error);
+    quasar.notify({
+      type: 'negative',
+      message: 'Niepoprawny identyfikator planu lekcji',
+    });
+    router.push({ name: routeNames.home });
+    client.value = undefined;
+  }
+}, { immediate: true });
 
-    if (window.sessionStorage.getItem('just-updated') !== null) {
-      window.sessionStorage.removeItem('just-updated');
-      quasar.notify({
-        icon: 'arrow_circle_up',
-        message: 'Zaktualizowano aplikację!',
-        caption: 'Korzystasz teraz z najnowszej wersji',
-        timeout: 6000,
-      });
-    }
+if (window.sessionStorage.getItem('just-updated') !== null) {
+  window.sessionStorage.removeItem('just-updated');
+  quasar.notify({
+    icon: 'arrow_circle_up',
+    message: 'Zaktualizowano aplikację!',
+    caption: 'Korzystasz teraz z najnowszej wersji',
+    timeout: 6000,
+  });
+}
 
-    watch(
-      () => config.dark,
-      (value) => quasar.dark.set(value),
-      { immediate: true },
-    );
-    return {
-      isOldDomian: computed(() => window.location.host === process.env.OLD_HOST),
-    };
-  },
-});
+watch(
+  () => config.dark,
+  (value) => quasar.dark.set(value),
+  { immediate: true },
+);
+const isOldDomian = computed(() => window.location.host === process.env.OLD_HOST);
 </script>
