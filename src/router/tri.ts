d9b57e6eda45812b3b9c4882dc871da4a5d4d3d4
Dominik Korsa <dominik.korsa@gmail.com>
2023-09-09 19:49:51 +0200

    Disable support for new TRIs

--- src/router/tri.ts
+++ src/router/tri.ts
@@ -1,7 +1,7 @@
 const anyOf = (...parts: string[]) => `(?:${parts.join('|')})`;
-const optional = (part: string) => `(?:${part})?`;
-
-const timetableLiteral = 'plan';
+// const optional = (part: string) => `(?:${part})?`;
+//
+// const timetableLiteral = 'plan';
 
 const schoolSlugs = {
   'v-lo': 4735,
@@ -11,33 +11,32 @@ const vLo = anyOf(...Object.keys(schoolSlugs));
 
 const optivum = 'o,[012],[^\\/,]+,[^\\/,]+';
 
-const rspo = '\\d+';
+// const rspo = '\\d+';
 
-const slash = '\\/';
-const slug = '[^\\/\\.,]+';
-const part = (prefix: string) => `${prefix}\\.${slug}`;
+// const slash = '\\/';
+// const slug = '[^\\/\\.,]+';
+// const part = (prefix: string) => `${prefix}\\.${slug}`;
 
-const voivodeship = part('w') + slash;
-const county = part('p') + slash;
-const city = part('m');
-const commune = part('gm');
-const schoolOrRspo = slug;
-
-const url = '\'[^\']+\'';
-
-const version = '\\+\\d+';
+// const voivodeship = part('w') + slash;
+// const county = part('p') + slash;
+// const city = part('m');
+// const commune = part('gm');
+// const schoolOrRspo = slug;
+//
+// const url = '\'[^\']+\'';
+//
+// const version = '\\+\\d+';
 
 export const triRegex = anyOf(
   vLo,
   optivum,
-  url,
-  timetableLiteral + version,
-  anyOf(
-    rspo,
-    optional(voivodeship) + anyOf(
-      optional(county) + city,
-      county + commune,
-    ) + slash + schoolOrRspo,
-  ) + optional(version),
+  // url,
+  // timetableLiteral + version,
+  // anyOf(
+  //   rspo,
+  //   optional(voivodeship) + anyOf(
+  //     optional(county) + city,
+  //     county + commune,
+  //   ) + slash + schoolOrRspo,
+  // ) + optional(version),
 );
-console.log('Tri regex:', triRegex);
